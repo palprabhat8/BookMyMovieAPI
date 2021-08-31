d075dd2d124637ab88f61a594a6d53e1e593ab5c
palprabhat8 <palprabhat8@gmail.com>
2021-08-31 18:41:40 +0530

    editing urls

--- BookMyMovie/urls.py
+++ BookMyMovie/urls.py
@@ -19,11 +19,10 @@ from dashboard.views import MovieListView, CinemaListView, movie_in_city, movie_
 from booking.views import book_ticket
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', MovieListView.as_view(), name="movie-running"), 
+    	path('admin/', admin.site.urls),
+    	path('', MovieListView.as_view(), name="movie-running"), 
 	path('cinema_list/', CinemaListView.as_view(), name="cinema-list"),
 	path('movie/city/<str:city_name>', movie_in_city, name="movie-by-city"), #1. all movie playinh in my city
-	path('movie/cinema/<int:pk>', CinemaListView.as_view(), name="cinema-list"),
 	path('shows/movie/<int:pk>', movie_in_cinema, name="movie-by-cinema"), #2.all cinema in which a movie playing along with showtime
 	path('shows/<movie_name>/<cinema_name>', seat_available_for_show, name="seats-for-show"),#3.for each show check seats avalible
 	path('book/seats/<int:show_id>', book_ticket, name="book-ticket"),#4.book a ticket
